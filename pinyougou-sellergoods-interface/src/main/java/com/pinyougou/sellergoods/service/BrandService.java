208581e760babcb090073eb6724b6b7e83ea8957
feng <123456@uu.com>
2018-09-15 19:35:29 +0800

    第一天代码

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,18 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌接口
+ * @author Administrator
+ *
+ */
+public interface BrandService {
+	
+	public List<TbBrand> findAll();
+	
+	
+
+}
